63d64263bb5ed263c134a0269f980f65968529f1
Jeff <jbridges1119@gmail.com>
2022-09-15 14:15:00 -0600

    Add code for ping that returns info

--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
-namespace HelloWorld
+﻿using System.Net.NetworkInformation;
+using System.Text;
+//Create pingSender from a class and type it to the ping class
+Ping pingSender = new Ping();
+PingOptions options = new PingOptions();
+
+options.DontFragment = true;
+
+string data = "Learn to code";
+byte[] buffer = Encoding.ASCII.GetBytes(data);
+int timeout = 120;
+string address = "4.2.2.2";
+PingReply reply = pingSender.Send(address, timeout, buffer, options);
+//reply has the .notations from pingSender which we created with the Ping class
+//IpStatus - object Microsoft gave us 
+if (reply.Status == IPStatus.Success)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+  Console.WriteLine("Response: {0}", reply.Status.ToString());
+  Console.WriteLine("RoundTrip: {0}", reply.RoundtripTime);
+   Console.WriteLine("Time to live: {0}", reply.Options?.Ttl);
+    Console.WriteLine("Buffer size: {0}", reply.Buffer.Length);
 }
